44d8fad46bc6c5c00806e3da1855dd573e2dd589
simon71 <simon.harvey@moneysupermarket.com>
2016-12-11 16:26:03 +0000

    updated weather.py

--- weather.py
+++ weather.py
@@ -1,49 +1,102 @@
+import json
 import urllib
-import urllib.request
+# import urllib.request
 from bs4 import BeautifulSoup
-import requests
+# import requests
 from datetime import date
 
-
 #build api url
-t = str(date.today())
-urla ="http://www.bbc.co.uk/weather/en/2644037/daily/"
-urlb ="?day="
+# t = str(date.today())
 
-url = urla+t+urlb+"0"
-page = urllib.request.urlopen(url)
+def hours(s):
+    return [x.string+":00" for x in s.find('tr', class_="time").findAll("span", class_="hour")]
+
+def conditions(s):
+    return [x['title'] for x in s.find('tr', class_='weather-type').findAll('img')]
+
+def fatal(s):
+    sys.exit(s)
+
+def day_img(condition):
+    return {
+            'Clear Sky':'sun.png',
+            'Sunny':'sun.png',
+            'Sunny Intervals':'sunny_intervals.png',
+
+            'Partly Cloudy':'light_cloud.png',
+            'Light Cloud':'light_cloud.png',
+            'Grey Cloud':'light_cloud.png',
+            'Mist':'mist.png',
+            'Fog':'fog.png',
+
+            'Thundery Shower':'heavy_thunder_showers.png',
+            'Heavy Thunder':'heavy_thunder.png',
+            'Drizzle':'light_rain.png',
+            'Light Rain':'light_rain.png',
+            'Light Rain Shower':'light_rain_showers.png',
+
+            'Heavy Rain':'heavy_rain.png',
+            'Heavy Rain Shower':'heavy_rain_showers.png',
+
+            'Sleet':'sleet.png',
+            'Light Snow':'light_snow.png',
+            'Light Snow Shower':'light_snow_showers.png',
+            'Light Snow Rain Showers':'light_snow_rain_showers.png',
+            'Heavy Snow':'heavy_snow.png',
+            'Heavy Hail':'heavy_hail.png',
+        }.get(condition, None)
+
+def night_img(condition):
+    return {
+            'Clear Sky':'night_clear_sky.png',
+
+            'Partly Cloudy':'night_partly_cloudy.png',
+            'Light Cloud':'night_partly_cloudy.png',
+            'Mist':'mist.png',
+            'Fog':'fog.png',
+
+            'Heavy Thunder':'night_heavy_thunder.png',
+            'Drizzle':'night_light_showers.png',
+            'Light Rain':'night_light_showers.png',
+            'Light Rain Shower':'night_light_showers.png',
+
+            'Heavy Rain':'night_heavy_showers.png',
+            'Heavy Rain Shower':'night_heavy_showers.png',
+
+            'Sleet':'sleet.png',
+            'Light Snow':'night_light_snow.png',
+            'Light Snow Shower':'night_light_snow.png',
+            'Light Snow Rain Showers':'night_snow_rain_showers.png',
+            'Heavy Snow':'night_heavy_snow.png',
+            'Heavy Hail':'night_heavy_hail.png',
+        }.get(condition, None)
+
+url = "http://www.bbc.co.uk/weather/en/2644037/daily/" + date.today().strftime("%Y-%m-%d")
+page = urllib.urlopen(url)
 soup = BeautifulSoup(page, "html5lib")
+#table = soup.find("table", {'class':'weather moving-window three-hourly-0 one-hourly-21'})
 
-#sun rise and sunset
-# rise_set = soup.find('div', attrs={'class':'sunrise-sunset'})
-# sunrise = rise_set.find('span', attrs={'class':'sunrise'}).text
-# sunset = rise_set.find('span', attrs={'class':'sunset'}).text
+hrs = hours(soup)
+conditions = conditions(soup)
+if len(hrs) != len(conditions):
+    fatal('Unable to display weather at this moment in time. Please check back later')
 
-#last updated
-# a = soup.find('div', attrs={'class':'detail-container group'})
-# last_update = a.p.text
+day_hrs = ["05:00","06:00","07:00","08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00"]
 
-#splits table up
-table_head = soup.thead
-table_main = soup.tbody
-table_foot = soup.tfoot
+# dictionary where the key is the hour, and the value is the weather condition
+# at that time.
+weather_cond = dict(zip(hrs, conditions))
 
-#time
-# thead_rows = table_head.findAll('tr')
-# for h_tr in thead_rows:
-#     h_td = h_tr.findAll('th')
-#     time = [i.text.replace('\xa0hours', '').strip() for i in h_td]
+# dictionary where the key is the hour, and the value is the name of the image
+# representng the weather condition
+weather_img = {}
+for h in range(len(hrs)):
+    if hrs[h] in day_hrs:
+        img = day_img
+    else:
+        img = night_img
 
+    weather_img[hrs[h]] = img(conditions[h])
 
-#conditions
-#conditions = [img['alt'] for img in table_main.select('img[alt]')]
-
-#Temp
-c = table_main.findAll('span', attrs={'class':'units-value temperature-value temperature-value-unit-c'})
-c_temp = 
-# f = table_main.find('span', attrs={'class':'units-value temperature-value temperature-value-unit-f'}).text
-
-#wind
-wind = table_main.find('span', attrs={'class':'units-value windspeed-value windspeed-value-unit-kph'}).text
-
-print(c)
+print(json.dumps(weather_cond, indent=4))
+print(json.dumps(weather_img, indent=4))
