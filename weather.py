e41a8ce290b6afb0f39cd1dcf188583189cf5ad9
simon71 <simon.harvey@moneysupermarket.com>
2017-01-09 10:19:24 +0000

    updated html

--- weather.py
+++ weather.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 from datetime import date
 import time
 import os
+from collections import OrderedDict
+
 
 def hours(s):
     return [x.string+":00" for x in s.find('tr', class_="time").findAll("span", class_="hour")]
@@ -70,7 +72,7 @@ def night_img(condition):
             'Heavy Hail':'night_heavy_hail.png',
         }.get(condition, None)
 
-url = "http://www.bbc.co.uk/weather/en/2644037/daily/" + date.today().strftime("%Y-%m-%d")
+url = "http://www.bbc.co.uk/weather/en/2644037/"
 page = urllib.request.urlopen(url)
 soup = BeautifulSoup(page, "html5lib")
 #table = soup.find("table", {'class':'weather moving-window three-hourly-0 one-hourly-21'})
@@ -120,18 +122,15 @@ for h in weather_cond:
     else:
         weather_img[h] = (night_img(weather_cond[h]))
 
+weather_data_post= {}
 for i in hrs:
     if i > presentHour:
-        weather_data_post= {}
         weather_data_post[i] = weather_cond[i], weather_img[i], weather_temp[i], weather_humidity[i], weather_wind_speed[i], weather_wind_direction[i]
 
+weather_data_pre= {}
 for i in hrs:
     if i < presentHour:
-        weather_data_pre= {}
         weather_data_pre[i] = weather_cond[i], weather_img[i], weather_temp[i], weather_humidity[i], weather_wind_speed[i], weather_wind_direction[i]
-
-
-
 
 
 #writes the json data to file
@@ -147,3 +146,80 @@ sun_rise = soup.find('div', {'class':'sunrise-sunset'}).find('span', {'class':'s
 
 #returns the sun set time from the BBC weather website
 sun_set = soup.find('div', {'class':'sunrise-sunset'}).find('span', {'class':'sunset'}).text.strip()
+
+###############################################################
+#tomorrows data
+###############################################################
+
+url = "http://www.bbc.co.uk/weather/en/2644037/?day=1"
+page = urllib.request.urlopen(url)
+soup = BeautifulSoup(page, "html5lib")
+#table = soup.find("table", {'class':'weather moving-window three-hourly-0 one-hourly-21'})
+
+hrs = hours(soup)
+condition = conditions(soup)
+if len(hrs) != len(condition):
+    fatal('Unable to display weather at this moment in time. Please check back later')
+
+day_hrs = ["05:00","06:00","07:00","08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00"]
+
+presentHour = time.strftime('%H')+':00'
+
+# dictionary where the key is the hour, and the value is the weather condition
+# at that time.
+weather_cond = dict(zip(hrs, condition))
+
+#OrderedDict(sorted(item.iteritems(), key=lambda (k, v): sort_order.index(k)))
+# dictionary where the key is the hour, and the value is the name of the image
+# representng the weather condition
+
+#########################
+#temperature
+#########################
+temp = [x.contents[0] for x in soup.find('tr', {'class':'temperature'}).findAll('span', {'class': 'units-value temperature-value temperature-value-unit-c'})]
+#dict of hour and temp
+weather_temp = dict(zip(hrs, temp))
+
+#########################
+#humidity
+#########################
+humidity = [x.string.strip() for x in soup.find('tr', {'class': 'humidity'}).findAll('td', {'class':'value hours-1'})]
+#dict hours and humidity
+weather_humidity = dict(zip(hrs, humidity))
+
+###########################
+#wind speed
+###########################
+windS = [x.contents[0].strip() for x in soup.find('tr', {'class':'windspeed'}).findAll('span', {'class':'units-value windspeed-value windspeed-value-unit-mph'})]
+weather_wind_speed = dict(zip(hrs, windS))
+
+windD = [x.string for x in soup.find('tr', {'class':'wind-direction'}).findAll('abbr')]
+weather_wind_direction = dict(zip(hrs, windD))
+
+weather_img = {}
+for h in weather_cond:
+    if h in day_hrs:
+        weather_img[h] = (day_img(weather_cond[h]))
+    else:
+        weather_img[h] = (night_img(weather_cond[h]))
+
+
+weather_data_tomorrow_post= {}
+for i in hrs:
+    if i > "05:00":
+        weather_data_tomorrow_post[i] = weather_cond[i], weather_img[i], weather_temp[i], weather_humidity[i], weather_wind_speed[i], weather_wind_direction[i]
+
+weather_data_tomorrow_pre= {}
+for i in hrs:
+    if i < "06:00":
+        weather_data_tomorrow_pre[i] = weather_cond[i], weather_img[i], weather_temp[i], weather_humidity[i], weather_wind_speed[i], weather_wind_direction[i]
+
+
+
+#writes the json data to file
+with open('weather_data_tomorrow_post.json', 'w') as outfile:
+    json.dump(weather_data_tomorrow_post, outfile, indent=4, sort_keys=False)
+
+#writes the json data to file
+with open('weather_data_tomorrow_pre.json', 'w') as outfile:
+    json.dump(weather_data_tomorrow_pre, outfile, indent=4, sort_keys=False)
